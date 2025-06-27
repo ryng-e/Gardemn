a9db36b7beefeb6e80c844e45412dd7339419418
Luca <lucacerretini03@gmail.com>
2025-06-27 18:24:39 +0200

    Updated Service Worker

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
 	import '../app.css';
+	if ('serviceWorker' in navigator) {
+  		navigator.serviceWorker.register('/service-worker.js')
+    	.then(reg => console.log('Service Worker registrato:', reg))
+    	.catch(err => console.error('Errore registrazione SW:', err));
+	}
 	
 	let { children } = $props();
 </script>
